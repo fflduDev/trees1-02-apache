701ded0794caacd22d5e7dda0fcc022757f9b937
Joe Bally <joseph.bally@student.fairfield.edu>
2026-03-27 13:14:17 -0400

    Methods

--- OrgChartImpl.java
+++ OrgChartImpl.java
@@ -8,5 +8,33 @@ public class OrgChartImpl implements OrgChart{
 	//Employee is your generic 'E'..
 	private List<GenericTreeNode<Employee>> nodes = new ArrayList<>();
 	
+	public void addRoot(Employee e){
+
+	}
+		// if there is no orgchart, start it
+	
+	
+	public void clear(){
+
+	}
+		// get rid of the org chart
+	
+	public void addDirectReport(Employee manager, Employee newPerson){
+
+	}
+		// add the newPerson as a direct report (child) of manager
+	
+	public void removeEmployee(Employee firedPerson){
+
+	}
+		// remove the employee, give their direct reports to their supervisor
+	
+	public void showOrgChartDepthFirst(){
+
+	}
+	
+	public void showOrgChartBreadthFirst(){
+		
+	}
 	
 }
