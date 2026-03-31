b102fb0829856fbf2cb53c80b3144ae886a47072
Mike48200 <michaelconsidine2005@gmail.com>
2026-03-31 13:33:48 -0400

    removeChild

--- GenericTreeNode.java
+++ GenericTreeNode.java
@@ -15,10 +15,35 @@ public class GenericTreeNode<E> {
 	}
 	
 	public void removeChild(E theItem) {
+		int i = 0;
+
+		while(!children.get(i).equals(theItem) && i < children.size()) {
+			i++;
+		}
+
+		if(i == children.size()){
+			System.out.println("Item not found.");
+			return;
+		}
+
+		GenericTreeNode<E> curr = children.get(i);
+
+		if(curr.children.isEmpty()) {
+			children.remove(curr);
+			return;
+		}
+
+		int j = 0;
+
+		while(j < curr.children.size()) {
+			addChild(curr.children.get(j));
+			j++;
+		}
+
+		children.remove(curr);
+		
 		// this one is a little harder.
 		// what do you do when the item has children?
 		// I suggest "give them to the parent"
 	}
-	
-	
 } 
